e5c238eae28cd54fbf2ee9f94e66d8d1bb580151
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-10 17:16:29 -0500

    update routes category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { json } = require('sequelize/types');
 const { Category, Product, Tag } = require('../../models');
+const { restore } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
@@ -35,12 +37,24 @@ router.get('/:id', async(req, res) => {
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async(req, res) => {
+ // create a new category
+ try{
+  const categorynData = await Category.create(req.body);
+  res.status(200),json(err);
+ }catch (err){
+  res.status(400).json(err);
+ }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a category by its `id` value
+  try{
+  const categorynData =await Category.findByPk(req.params.id);
+  res.status(200).json(categorynData);
+  } catch (err){
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
