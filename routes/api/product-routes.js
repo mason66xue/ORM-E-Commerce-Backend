05b216227a591a2bf0833fe16e5235d7f49be098
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-10 20:27:09 -0500

    category update

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,27 +4,47 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
+  try{
+    const productData= await Product.findAll({
+      include: [{model:Category },{model: Tag}],
+    });
+    res.status(200).json(productData);
+  }catch(err){
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const productData = await Category.findByPk(req.params.id,{
+      include: [{model: Category},{model: Tag}],
+    });
+    if (!productData){
+      res.status(404).json({message: 'No id'});
+      return; 
+    }
+    
+    res.status(200).json(productData);
+    }catch(err){
+      res.status(500).json(err);
+    }
 });
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+router.post('/', async(req, res) => {
+  try{
+    const productData = await Product.create(req.body);
+    res.status(200),json(err);
+   }catch (err){
+    res.status(400).json(err);
+   } 
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
