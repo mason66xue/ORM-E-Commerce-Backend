02a545d13e2942c5871f6c4dea6150c4c4dde707
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-17 22:19:57 -0500

    update category-route.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
-const { json } = require('sequelize/types');
-const { Category, Product, Tag } = require('../../models');
-const { restore } = require('../../models/Product');
+const { Category, Product } = require('../../models');
+
 
 // The `/api/categories` endpoint
 
@@ -22,7 +21,7 @@ router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   try{
     const categorynData = await Category.findByPk(req.params.id,{
-      include: [{model: Product},],
+      include: [{model: Product}],
     });
     if (!categorynData){
       res.status(404).json({message: 'No id'});
@@ -40,8 +39,11 @@ router.get('/:id', async(req, res) => {
 router.post('/', async(req, res) => {
  // create a new category
  try{
-  const categorynData = await Category.create(req.body);
-  res.status(200),json(err);
+  const categorynData = await Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name,
+  });
+  res.status(200),json(categorynData);
  }catch (err){
   res.status(400).json(err);
  }
@@ -50,24 +52,30 @@ router.post('/', async(req, res) => {
 router.put('/:id', async(req, res) => {
   // update a category by its `id` value
   try{
-  const categorynData =await Category.findByPk(req.params.id);
+  const categorynData =await Category.update(req.body, {
+    where:{
+      id: req.params.id
+    }
+  });
+  if(!categorynData[0]){
+    res.status(404).json({message:'NO category with this id'});
+  };
   res.status(200).json(categorynData);
   } catch (err){
     res.status(400).json(err);
-  }
+  };
 });
 
 router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
 try{
-  const categorynData = await Category.update(req.body,{
+  const categorynData = await Category.destroy({
     where:{
       id: req.params.id,
-    },
-    individualHooks: true
+    }
   });
-  if (!categorynData[0]){
-    res.status (404).json({message:'No id'});
+  if (!categorynData){
+    res.status (404).json({message:'No category with this id'});
     return;
   }
   res.status (200).json(categorynData);
