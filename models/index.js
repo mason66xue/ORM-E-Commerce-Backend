b004a760e3cea6739d87b3e00efc1bd9c55a681b
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-01-17 22:04:08 -0500

    revise index.js

--- models/index.js
+++ models/index.js
@@ -3,12 +3,12 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-const { belongsToMany } = require('./Product');
+
 
 // Products belongsTo Category
-Product.hasOne(Category,{
+Product.belongsTo(Category,{
 foreignKey: 'category_id',
-onDelete: 'CASCADE'
+
 
 });
 
@@ -21,15 +21,21 @@ Category.hasMany(Product,{
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
-  through: ProductTag,
   foriegnKey: 'product_id',
+  through:{
+    model: ProductTag,
+    unique: false
+  }
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
-  through: ProductTag,
   foreignKey: 'tag_id',
-})
+  through:{
+    model: ProductTag,
+    unique:false
+  }
+});
 
 module.exports = {
   Product,
